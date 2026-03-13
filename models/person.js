4706d3d5b571ed9d1ec4c7327f060eebaaaea244
Trang <trangvu197@gmail.com>
2026-03-13 18:44:56 +0200

    updates

--- models/person.js
+++ models/person.js
@@ -27,7 +27,7 @@ const personSchema = new mongoose.Schema({
       },
       message: props => `${props.value} is not a valid phone number!`
     },
-    required: [true, 'User phone number required']
+    required: [true, 'Phone number required']
   },
 })
 
